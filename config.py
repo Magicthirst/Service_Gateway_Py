aee723de040cb3757dd57886288cb81dbffa0191
yoloroy <slart2108@gmail.com>
2025-07-13 21:51:44 +0300

    :construction_worker: Dockerize

--- config.py
+++ config.py
@@ -3,14 +3,18 @@ import os
 from httpx import AsyncClient
 
 
+def not_found_env(variable_name):
+    raise Exception(f'not found env: {variable_name=}')
+
+
 protocol = 'http'
-host = os.environ.get("HOST") or '0.0.0.0'
-port = os.environ.get("PORT") or 8002
+host = os.environ.get("IP") or not_found_env()
+port = int(os.environ.get("PORT")) or not_found_env()
 
 run_url = f'{protocol}://{host}:{port}'
-auth_url = os.environ.get("AUTH_URL") or 'http://localhost:8000'
-hosts_url = os.environ.get("HOSTS_URL") or 'http://localhost:8001'
-sync_outlet_url = os.environ.get("SYNC_OUTLET_URL") or 'localhost:8765'
-sync_main_url = os.environ.get("SYNC_MAIN_URL") or 'localhost:8766'
+auth_url = os.environ.get("AUTH_URL") or not_found_env()
+hosts_url = os.environ.get("HOSTS_URL") or not_found_env()
+sync_outlet_url = os.environ.get("SYNC_OUTLET_URL") or not_found_env()
+sync_main_url = os.environ.get("SYNC_MAIN_URL") or not_found_env()
 
 async_client = AsyncClient()
